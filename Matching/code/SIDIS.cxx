8b7e02927c23d5c2abf38ada1fd10e6e8c732310
Valerio Bertone <valerio.bertone@cern.ch>
2017-12-30 16:30:22 +0100

    adjusting code

--- Matching/code/SIDIS.cxx
+++ Matching/code/SIDIS.cxx
@@ -67,25 +67,11 @@ double BLFOgq(double const& x, double const& z)
 }
 
 int main() {
-  // Kinematics in terms of the variables in which TIMBA is
-  // differential.
-  const double EP  = 820;
-  const double El  = 27.6;
-  const double VS  = sqrt( 4 * EP * El );
-  const double x   = 5e-5;
-  const double Q2  = 2;
-  const double pT  = 3.5;
-  const double eta = 0;
-
-  // Internal kinematic variables
-  const double Q  = sqrt(Q2);
-  const double y  = Q2 / x / VS / VS;
-  const double qT = exp(-eta) * VS * sqrt( y * ( 1 - x ) );
-  const double z  = pT / qT;
-
   // PDF and FF sets
-  LHAPDF::PDF* PDFs = LHAPDF::mkPDF("NNPDF31_nlo_as_0118");
-  LHAPDF::PDF* FFs  = LHAPDF::mkPDF("NNFF10_HadronSum_nlo");
+  //LHAPDF::PDF* PDFs = LHAPDF::mkPDF("NNPDF31_nlo_as_0118");
+  //LHAPDF::PDF* FFs  = LHAPDF::mkPDF("NNFF10_HadronSum_nlo");
+  LHAPDF::PDF* PDFs = LHAPDF::mkPDF("CT14nlo");
+  LHAPDF::PDF* FFs  = LHAPDF::mkPDF("DSS07_NLO_HadronSum");
 
   // Heavy quark masses.
   const double mc = PDFs->quarkThreshold(4);
@@ -93,54 +79,77 @@ int main() {
   const double mt = PDFs->quarkThreshold(6);
   const vector<double> Masses{0, 0, 0, mc, mb, mt};
 
-  // Useful definitions
-  const double qToQ2  = qT * qT / Q2;
-  const double zmax   = ( 1 - x ) / ( 1 - x * ( 1 - qToQ2 ) );
-  const double y2     = y * y;
-  const double Yp     = 1 + pow(1-y, 2);
-  const double nf     = NF(Q, Masses);
-  const double as     = PDFs->alphasQ(Q) / FourPi;
-  const double alpha2 = pow(1./137,2);
-  const double hc2    = 0.38942957e6;
-
-  // Define integrands.
-  const auto Xsec = [=] (double const& zb) -> double
+  const auto SIDISxsec = [=] (double const& VS, double const& x, double const& Q2, double const& pT, double const& eta) -> double
     {
-      const double xb0 = ( 1 - zb ) / ( 1 - zb * ( 1 - qToQ2 ) );
-      const double xt  = x / xb0;
-      const double zt  = z / zb;
+      // Internal kinematic variables
+      const double Q  = sqrt(Q2);
+      const double y  = Q2 / x / VS / VS;
+      const double qT = exp(-eta) * VS * sqrt( y * ( 1 - x ) );
+      const double z  = pT / qT;
 
-      // Distribution combinations
-      double qq = 0;
-      double qg = 0;
-      double gq = 0;
-      const double gPDF = PDFs->xfxQ(21,xt,Q);
-      const double gFF  = FFs->xfxQ(21,zt,Q);
-      for (int i = 1; i <= nf; i++)
-	{
-	  qq += QCh2[i-1] * ( PDFs->xfxQ(i,xt,Q) * FFs->xfxQ(i,zt,Q) + PDFs->xfxQ(-i,xt,Q) * FFs->xfxQ(-i,zt,Q) );
-	  qg += QCh2[i-1] * gPDF * ( FFs->xfxQ(i,zt,Q) + FFs->xfxQ(-i,zt,Q) );
-	  gq += QCh2[i-1] * ( PDFs->xfxQ(i,xt,Q) + PDFs->xfxQ(-i,xt,Q) ) * gFF;
-	}
-      qq /= xt * zt;
-      qg /= xt * zt;
-      gq /= xt * zt;
+      // Useful definitions
+      const double qToQ2  = qT * qT / Q2;
+      const double zmax   = ( 1 - x ) / ( 1 - x * ( 1 - qToQ2 ) );
+      const double y2     = y * y;
+      const double Yp     = 1 + pow(1-y, 2);
+      const double nf     = NF(Q, Masses);
+      const double as     = PDFs->alphasQ(Q) / FourPi;
+      const double alpha2 = pow(1./137,2);
+      const double hc2    = 0.38942957e6;
 
-      // Construct integrand
-      const double integrand = as * x * xb0 *
-      ( ( B2FOqq(xb0, zb, qT, Q) - y2 / Yp * BLFOqq(xb0, zb) ) * qq
-      + ( B2FOqg(xb0, zb, qT, Q) - y2 / Yp * BLFOqg(xb0, zb) ) * qg
-      + ( B2FOgq(xb0, zb, qT, Q) - y2 / Yp * BLFOgq(xb0, zb) ) * gq ) / Q / Q / ( 1 - zb );
+      // Define integrands.
+      const auto Xsec = [=] (double const& zb) -> double
+      {
+	const double xb0 = ( 1 - zb ) / ( 1 - zb * ( 1 - qToQ2 ) );
+	const double xt  = x / xb0;
+	const double zt  = z / zb;
+	if (xt >= 1 || zt >=1)
+	  return 0;
 
-      return 2 * pT * hc2 * 2 * M_PI * alpha2 * Yp * integrand / z / x / pow(Q,4);
+	// Distribution combinations
+	double qq = 0;
+	double qg = 0;
+	double gq = 0;
+	const double gPDF = PDFs->xfxQ(21,xt,Q);
+	const double gFF  = FFs->xfxQ(21,zt,Q);
+	for (int i = 1; i <= nf; i++)
+	  {
+	    qq += QCh2[i-1] * ( PDFs->xfxQ(i,xt,Q) * FFs->xfxQ(i,zt,Q) + PDFs->xfxQ(-i,xt,Q) * FFs->xfxQ(-i,zt,Q) );
+	    qg += QCh2[i-1] * gPDF * ( FFs->xfxQ(i,zt,Q) + FFs->xfxQ(-i,zt,Q) );
+	    gq += QCh2[i-1] * ( PDFs->xfxQ(i,xt,Q) + PDFs->xfxQ(-i,xt,Q) ) * gFF;
+	  }
+	qq /= xt * zt;
+	qg /= xt * zt;
+	gq /= xt * zt;
+
+	// Construct integrand
+	const double integrand = as * x * xb0 *
+	( ( B2FOqq(xb0, zb, qT, Q) - y2 / Yp * BLFOqq(xb0, zb) ) * qq
+	  + ( B2FOqg(xb0, zb, qT, Q) - y2 / Yp * BLFOqg(xb0, zb) ) * qg
+	  + ( B2FOgq(xb0, zb, qT, Q) - y2 / Yp * BLFOgq(xb0, zb) ) * gq ) / Q / Q / ( 1 - zb );
+
+	return 2 * pT * hc2 * 2 * M_PI * alpha2 * Yp * integrand / z / x / pow(Q,4);
+      };
+
+      // Integrate
+      // Integrate also over x
+      const Integrator Integral{Xsec};
+      return Integral.integrate(z, zmax, 1e-5);
     };
 
-  // Integrate
-  // Integrate also over x
-  const Integrator Integral{Xsec};
-  const double value = Integral.integrate(z, zmax, 1e-5);
+  // Kinematics in terms of the variables in which TIMBA is
+  // differential.
+  const double EP  = 820;
+  const double El  = 27.6;
+  const double VS  = sqrt( 4 * EP * El );
+  const double x   = 5e-5;
+  const double Q2  = 2;
+  const vector<double> pTv = {3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5};
+  const double eta = 1;
 
-  cout << x << "  " << Q2 << "  " << pT << "  " << eta << "  " << value << endl;
+  cout << scientific;
+  for (auto const& pT : pTv)
+    cout << x << "  " << Q2 << "  " << pT << "  " << eta << "  " << SIDISxsec(VS, x, Q2, pT, eta) << endl;
 
   return 0;
 }
