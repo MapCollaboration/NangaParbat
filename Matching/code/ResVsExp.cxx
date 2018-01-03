4408bf21f80572476921c900921fcedc9795b668
Valerio Bertone <valerio.bertone@cern.ch>
2018-01-03 22:22:05 +0100

    working on the matching notes

--- Matching/code/ResVsExp.cxx
+++ Matching/code/ResVsExp.cxx
@@ -33,41 +33,23 @@
 #include "apfel/tools.h"
 #include "apfel/integrator.h"
 #include "apfel/doubleobject.h"
+#include "apfel/tmdbuilder.h"
+#include "apfel/ogataquadrature.h"
+#include "apfel/tmdcrosssections.h"
 
 using namespace std;
 using namespace apfel;
 
-double B2FOqq(double const& x, double const& z, double const& qT, double const& Q)
-{
-  return 2 * CF * ( ( 1 - x ) * ( 1 - z ) + 4 * x * z + ( 1 + x * x * z * z ) * Q * Q / x / z / qT / qT ); 
-}
-
-double B2FOqg(double const& x, double const& z, double const& qT, double const& Q)
-{
-  return 2 * TR * ( 8 * x * ( 1 - x ) + ( x * x + ( 1 - x ) * ( 1 - x ) ) * ( z * z + ( 1 - z ) * ( 1 - z ) ) * ( 1 - x ) * Q * Q / x / z / z / qT / qT ); 
-}
-
-double B2FOgq(double const& x, double const& z, double const& qT, double const& Q)
-{
-  return 2 * CF * ( ( 1 - x ) * z + 4 * x * ( 1 - z ) + ( 1 + x * x * ( 1 - z ) * ( 1 - z ) ) * ( 1 - z ) * Q * Q / x / z / z / qT / qT ); 
-}
-
-double BLFOqq(double const& x, double const& z)
-{
-  return 8 * CF * x * z;
-}
-
-double BLFOqg(double const& x, double const&)
-{
-  return 16 * TR * x * ( 1 -  x );
-}
-
-double BLFOgq(double const& x, double const& z)
-{
-  return 8 * CF * x * ( 1 - z );
-}
-
 int main() {
+  // Kinematics in terms of the variables in which TIMBA is
+  // differential.
+  const double EP = 820;
+  const double El = 27.6;
+  const double VS = sqrt( 4 * EP * El );
+  const double x  = 1e-3;
+  const double z  = 1e-1;
+  const double y  = 0.5;
+
   // PDF and FF sets
   //LHAPDF::PDF* PDFs = LHAPDF::mkPDF("NNPDF31_nlo_as_0118");
   //LHAPDF::PDF* FFs  = LHAPDF::mkPDF("NNFF10_HadronSum_nlo");
@@ -80,68 +62,81 @@ int main() {
   const double mt = PDFs->quarkThreshold(6);
   const vector<double> Masses{0, 0, 0, mc, mb, mt};
 
-  // Function for the computation of the fixed-order cross section at
-  // O(as).
-  const auto xsecFO = [=] (double const& VS, double const& x, double const& y, double const& z, double const& qT2) -> double
+  // Initialize TMD and DGLAP objects.
+  const Grid g{{SubGrid{100,1e-5,3}, SubGrid{60,1e-1,3}, SubGrid{50,5e-1,3}, SubGrid{60,7e-1,3}}};
+
+  // Alpha_s
+  const auto Alphas = [&] (double const& mu) -> double{ return PDFs->alphasQ(mu); };
+
+  // Rotate distributions into the QCD evolution basis.
+  const auto RotPDFs = [=] (double const& x, double const& mu) -> map<int,double>{ return PhysToQCDEv(PDFs->xfxQ(x,mu)); };
+  const auto RotFFs  = [=] (double const& x, double const& mu) -> map<int,double>{ return PhysToQCDEv(FFs->xfxQ(x,mu)); };
+
+  // Construct set of distributions as a function of the scale to be
+  // tabulated.
+  const auto EvolvedPDFs = [=,&g] (double const& mu) -> Set<Distribution>{ return Set<Distribution>{EvolutionBasisQCD{NF(mu, Masses)}, DistributionMap(g, RotPDFs, mu)}; };
+  const auto EvolvedFFs  = [=,&g] (double const& mu) -> Set<Distribution>{ return Set<Distribution>{EvolutionBasisQCD{NF(mu, Masses)}, DistributionMap(g, RotFFs, mu)}; };
+
+  // Tabulate PDFs
+  const TabulateObject<Set<Distribution>> TabPDFs{EvolvedPDFs, 50, PDFs->qMin(), PDFs->qMax(), 3, Masses};
+  const TabulateObject<Set<Distribution>> TabFFs{EvolvedFFs,   50, FFs->qMin(),  FFs->qMax(),  3, Masses};
+  const auto CollPDFs = [&] (double const& mu) -> Set<Distribution> { return TabPDFs.Evaluate(mu); };
+  const auto CollFFs  = [&] (double const& mu) -> Set<Distribution> { return TabFFs.Evaluate(mu); };
+
+  // Get relevant objects.
+  const auto PDFObj = InitializeDglapObjectsQCD(g, Masses);
+  const auto FFObj  = InitializeDglapObjectsQCDT(g, Masses);
+  const auto TmdObj = InitializeTmdObjects(g, Masses);
+
+  // Get TMD evolution factors.
+  const auto Mub = [] (double const& b) -> double{ return 2 * exp(- emc) / b; };
+  const auto EvolFactors = EvolutionFactors(TmdObj, Mub, Mub, 1, Alphas);
+
+  // Function for the computation of the resummed cross section.
+  const auto xsecRes = [=,&g] (double const& VS, double const& x, double const& y, double const& z, double const& qT2) -> double
     {
       // Internal kinematic variables
       const double Q2 = x * y * VS * VS;
       const double Q  = sqrt(Q2);
       const double qT = sqrt(qT2);
 
+      // Get matched TMD PDFs and FFs.
+      const auto Qb = [Q] (double const&) -> double{ return Q; };
+      const auto MatchedTmdPDFs = MatchTmdPDFs(TmdObj, PDFObj, CollPDFs, Qb, 1, Alphas);
+      const auto MatchedTmdFFs  = MatchTmdFFs(TmdObj,  FFObj,  CollFFs,  Qb, 1, Alphas);
+
       // Useful definitions
-      const double qToQ2  = qT2 / Q2;
-      const double zmax   = ( 1 - x ) / ( 1 - x * ( 1 - qToQ2 ) );
+      const double qToQ2  = qT * qT / Q2;
       const double y2     = y * y;
       const double Yp     = 1 + pow(1-y, 2);
       const int nf        = NF(Q, Masses);
       const double as     = PDFs->alphasQ(Q) / FourPi;
       const double alpha2 = pow(1./137,2);
+      const double L      = 4 * CF * ( - log(qToQ2) - 3. / 2 );
 
-      // Define integrands.
-      const auto Xsec = [=] (double const& zb) -> double
+      // Function to be integrated over b
+      const auto TMDLumib = [=] (double const& b) -> double
       {
-	const double xb0 = ( 1 - zb ) / ( 1 - zb * ( 1 - qToQ2 ) );
-	const double xt  = x / xb0;
-	const double zt  = z / zb;
-	if (xt >= 1 || zt >=1)
-	  return 0;
+	// Evolve TMDs.
+	const vector<double> ef = EvolFactors(b, Q, Q2);
+	const auto EvolvedTMDPDFs = ef * MatchedTmdPDFs(b);
+	const auto EvolvedTMDFFs  = ef * MatchedTmdFFs(b);
 
-	// Distribution combinations
-	double qq = 0;
-	double qg = 0;
-	double gq = 0;
-	const double gPDF = PDFs->xfxQ(21,xt,Q);
-	const double gFF  = FFs->xfxQ(21,zt,Q);
+	map<int,Distribution> PDFs = QCDEvToPhys(EvolvedTMDPDFs.GetObjects());
+	map<int,Distribution> FFs  = QCDEvToPhys(EvolvedTMDFFs.GetObjects());
+	double lumi = 0;
 	for (int i = 1; i <= nf; i++)
-	  {
-	    qq += QCh2[i-1] * ( PDFs->xfxQ(i,xt,Q) * FFs->xfxQ(i,zt,Q) + PDFs->xfxQ(-i,xt,Q) * FFs->xfxQ(-i,zt,Q) );
-	    qg += QCh2[i-1] * gPDF * ( FFs->xfxQ(i,zt,Q) + FFs->xfxQ(-i,zt,Q) );
-	    gq += QCh2[i-1] * ( PDFs->xfxQ(i,xt,Q) + PDFs->xfxQ(-i,xt,Q) ) * gFF;
-	  }
-	qq /= xt * zt;
-	qg /= xt * zt;
-	gq /= xt * zt;
-
-	// Construct integrand
-	const double integrand = as * x * xb0 *
-	( ( B2FOqq(xb0, zb, qT, Q) - y2 / Yp * BLFOqq(xb0, zb) ) * qq
-	  + ( B2FOqg(xb0, zb, qT, Q) - y2 / Yp * BLFOqg(xb0, zb) ) * qg
-	  + ( B2FOgq(xb0, zb, qT, Q) - y2 / Yp * BLFOgq(xb0, zb) ) * gq ) / Q / Q / ( 1 - zb );
-
-	return 2 * qT * 2 * M_PI * alpha2 * Yp * integrand / x / y / Q2;
+	  lumi += QCh2[i-1] * ( PDFs.at(i).Evaluate(x) * FFs.at(i).Evaluate(z) + PDFs.at(-i).Evaluate(x) * FFs.at(-i).Evaluate(z) );
+	return lumi / z;
       };
 
-      // Integrate over z.
-      const Integrator Integral{Xsec};
-      return Integral.integrate(z, zmax, 1e-5);
+      // Kinmatic factors inclusing the hard form factor.
+      const double hcs = 2 * qT * 2 * M_PI * alpha2 * Yp * HardCrossSectionSIDIS(1, as * FourPi, nf, 1) / x / y / Q2;
+
+      // Integrate over b.
+      return hcs * OgataQuadrature(TMDLumib, qT);
     };
 
-  // Initialize space- and time-like splitting functions.
-  const Grid g{{SubGrid{100,1e-5,3}, SubGrid{60,1e-1,3}, SubGrid{50,5e-1,3}, SubGrid{60,7e-1,3}}};
-  const auto PDFObj = InitializeDglapObjectsQCD(g, Masses);
-  const auto FFObj  = InitializeDglapObjectsQCDT(g, Masses);
- 
   // Function for the computation of the asymptotic cross section at
   // O(as).
   const auto xsecAsy = [=,&g] (double const& VS, double const& x, double const& y, double const& z, double const& qT2) -> double
@@ -184,15 +179,6 @@ int main() {
       return 2 * qT * 2 * M_PI * alpha2 * Yp * ( as * DoubDist.Evaluate(x,z) / qT2 / z ) / x / y / Q2;
     };
 
-  // Kinematics in terms of the variables in which TIMBA is
-  // differential.
-  const double EP = 820;
-  const double El = 27.6;
-  const double VS = sqrt( 4 * EP * El );
-  const double x  = 1e-3;
-  const double z  = 1e-1;
-  const double y  = 0.5;
-
   const int    nqT    = 200;
   const double qTmin  = 0.01;
   const double qTmax  = 100;
@@ -203,7 +189,7 @@ int main() {
   for (int iqT = 0; iqT < nqT; iqT++)
     {
       cout << qT << "  "
-	   << xsecFO(VS, x, y, z, qT) << "  "
+	   << xsecRes(VS, x, y, z, qT) << "  "
 	   << xsecAsy(VS, x, y, z, qT) << "  "
 	   << endl;
       qT *= qTstep;
--- Matching/plots/ResVsExp.py
+++ Matching/plots/ResVsExp.py
@@ -13,31 +13,31 @@ ax.get_xaxis().set_tick_params(which='major', size=10)
 ax.get_yaxis().set_tick_params(which='minor', size=5)
 ax.get_xaxis().set_tick_params(which='minor', size=5)
 
-data = np.loadtxt('FOvsAsy.dat')
+data = np.loadtxt('ResVsExp.dat')
 
-pl.title(r"Fixed order vs. asymptotic limit at $\mathcal{O}(\alpha_s)$", fontsize=22)
+pl.title(r"Resummed@NLL vs. expansion at $\mathcal{O}(\alpha_s)$", fontsize=22)
 
 pl.xlabel(r'$q_T$ [GeV]', fontsize=22, labelpad=-2)
 pl.ylabel(r'$\frac{d\sigma}{dx dy dz dq_T}$', fontsize=26, labelpad=-2.5)
 pl.xlim(0.01, 20)
-pl.ylim(0.02, 10)
+pl.ylim(0.01, 10)
 pl.yscale('log')
 pl.xscale('log')
 pl.xticks(size=20)
 pl.yticks(size=20)
 
-ax.text(0.012, 0.1024, r'$\sqrt{S} = 300.9$ GeV', fontsize=22)
-ax.text(0.012, 0.064,  r'$x = 10^{-3}$', fontsize=22)
-ax.text(0.012, 0.04,   r'$ z = 10^{-1}$', fontsize=22)
-ax.text(0.012, 0.025,  r'$y = 0.5$', fontsize=22)
+ax.text(0.012, 0.512, r'$\sqrt{S} = 300.9$ GeV', fontsize=22)
+ax.text(0.012, 0.32,  r'$x = 10^{-3}$', fontsize=22)
+ax.text(0.012, 0.2,   r'$ z = 10^{-1}$', fontsize=22)
+ax.text(0.012, 0.125,  r'$y = 0.5$', fontsize=22)
 
-ax.text(0.7, 0.04,  r'PDF set: CT14nlo', fontsize=22)
-ax.text(0.7, 0.025, r'FF set: DSS07 ($h+\overline{h}$)', fontsize=22)
+ax.text(0.7, 0.019,  r'PDF set: CT14nlo', fontsize=22)
+ax.text(0.7, 0.0125, r'FF set: DSS07 ($h+\overline{h}$)', fontsize=22)
 
-pl.plot(data[:,0], data[:,1], 'r-', label='Fixed order')
-pl.plot(data[:,0], data[:,2], 'b-', label='Asymptotic limit')
+pl.plot(data[:,0], data[:,1], 'r-', label='Resummed')
+pl.plot(data[:,0], data[:,2], 'b-', label='Expansion')
 
 pl.legend(fontsize=22)
 
-pl.savefig('FOvsAsy.pdf')
+pl.savefig('ResVsExp.pdf')
 pl.show()
