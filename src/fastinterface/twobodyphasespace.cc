531a4c040c69037573165ace49e4cf89ca073ad8
Valerio Bertone <valerio.bertone@cern.ch>
2019-06-26 22:34:16 +0100

    implementing corrected phase-space reduction factor

--- src/fastinterface/twobodyphasespace.cc
+++ src/fastinterface/twobodyphasespace.cc
@@ -8,7 +8,6 @@
 #include <math.h>
 #include <algorithm>
 #include <apfel/integrator.h>
-#include <iostream>
 
 namespace NangaParbat
 {
@@ -30,19 +29,23 @@ namespace NangaParbat
     // Useful definitions
     const double Q2     = Q * Q;
     const double qT2    = qT * qT;
+    const double qT4    = qT2 * qT2;
     const double pTmin2 = _pTmin * _pTmin;
     const double M2     = Q2 + qT2;
     const double M      = sqrt(M2);
     const double ctgh   = 1 / tanh(y - _etamax);
 
-    // Integrand function
-    const auto F = [&] (double const& eta) -> double
+    // Integrand function with g^{\mu\nu}
+    const auto IntegrandP = [&] (double const& eta) -> double
     {
       // Useful definitions
       const double ch    = cosh(eta - y);
+      const double sh2   = ch * ch - 1;
       const double Eq    = M * ch;
       const double Eq2   = Eq * Eq;
+      const double Eq4   = Eq2 * Eq2;
       const double EmqT2 = Eq2 - qT2;
+      const double EmqT4 = EmqT2 * EmqT2;
       const double EmqT  = sqrt(EmqT2);
 
       // Auxiliary functions
@@ -59,20 +62,28 @@ namespace NangaParbat
         return 0;
 
       // Primitive function of the intregration in cos(phi) (up to a
-      // factor that can be compute externally).
-      const auto F = [&] (double const& x) -> double
+      // factor that can be compute externally) when contractin the
+      // leptonic tensor with g^{\mu\nu}_\perp.
+      const auto Fbar = [&] (double const& x) -> double
       {
         const double x2 = x * x;
         const double xp = sqrt( 1 - x2 );
         const double Fi = qT2 * x * xp / ( x2 * qT2 - Eq2 )
         - Eq * ( atan( ( qT - x * Eq ) / EmqT / xp ) - atan( ( qT + x * Eq ) / EmqT / xp ) ) / EmqT;
-        return Fi;
+        const double Gi = Q2 * sh2
+	* ( qT2 * xp * ( ( ( 11 * Eq2 * qT2 + 4 * qT4 ) * x2 + 3 * Eq * qT * ( 9 * Eq2 + qT2 ) * x + 18 * Eq4 - 5 * Eq2 * qT2 + 2 * qT4 ) / pow(x * qT + Eq, 3) +
+			 ( ( 11 * Eq2 * qT2 + 4 * qT4 ) * x2 - 3 * Eq * qT * ( 9 * Eq2 + qT2 ) * x + 18 * Eq4 - 5 * Eq2 * qT2 + 2 * qT4 ) / pow(x * qT - Eq, 3) )
+        - 6 * Eq * ( 2 * Eq2 + 3 * qT2 )
+	* ( atan( ( qT - x * Eq ) / EmqT / xp ) - atan( ( qT + x * Eq ) / EmqT / xp ) ) / EmqT ) / EmqT4 / 4;
+
+        return 3 * Fi / 4 + Gi / 4;
+        //return Fi;
       };
-      return ( F(x2) - F(x1) ) / EmqT2;
+      return ( Fbar(x2) - Fbar(x1) ) / EmqT2;
     };
 
     // Return integral
-    const apfel::Integrator Ieta{F};
+    const apfel::Integrator Ieta{IntegrandP};
     return Q2 * Ieta.integrate(-_etamax, _etamax, _eps) / 4 / M_PI;
   }
 
