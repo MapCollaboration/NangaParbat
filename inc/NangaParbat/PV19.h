91865018bf2aff0c3b2ff7f5203923a534f908b1
Alessandro Bacchetta <alessandro.bacchetta@unipv.it>
2019-10-08 18:23:53 +0200

    New PV19.h

--- inc/NangaParbat/PV19.h
+++ inc/NangaParbat/PV19.h
@@ -1,6 +1,6 @@
 //
 // Author: Valerio Bertone: valerio.bertone@cern.ch
-//
+// Authro: Alessandro Bacchetta: alessandro.bacchetta@unipv.it
 
 #pragma once
 
@@ -19,7 +19,7 @@ namespace NangaParbat
   {
   public:
 
-    PV19(): Parameterisation{"PV19", 2, std::vector<double>{0.13, 0.285, 2.98, 0.173, 0.39, 0.}} { };
+    PV19(): Parameterisation{"PV19", 2, std::vector<double>{0.13, 0.285, 2.98, 0.173, 0.39, 0., 0.1, 0.1, 0., 0., 0., 0.1, 0., 1.}} { };
 
     double Evaluate(double const& x, double const& b, double const& zeta, int const& ifunc) const
     {
@@ -32,34 +32,46 @@ namespace NangaParbat
 
       // Free paraMeters
       const double g2     = this->_pars[0];
-      const double Npt    = this->_pars[1];
+      const double N1    = this->_pars[1];
       const double alpha  = this->_pars[2];
       const double sigma  = this->_pars[3];
-      //const double lambda = this->_pars[4];
-      const double delta  = this->_pars[5];
+      const double delta  = this->_pars[4];
+      const double lambdaB = this->_pars[5];
+      const double N1B    = this->_pars[6];
+      const double alphaB  = this->_pars[7];
+      const double sigmaB  = this->_pars[8];
+      const double deltaB  = this->_pars[9];
+      const double lambdaC = this->_pars[10];
+      const double g1C    = this->_pars[11];
+      const double g2B    = this->_pars[12];
+      const double beta     = this->_pars[13];
 
       // TMD PDFs
       const double Q02 = 1;
       const double xhat = 0.1;
-      const double g1   = Npt * ( pow(x, sigma) + delta ) / ( pow(xhat, sigma) + delta ) * pow((1 - x) / (1 - xhat), alpha);
+      const double g1   = N1 * ( pow(x, sigma) + delta ) / ( pow(xhat, sigma) + delta ) * pow((1 - x) / (1 - xhat), alpha);
+      const double g1B   = N1B * ( pow(x, sigmaB) + deltaB ) / ( pow(xhat, sigmaB) + deltaB ) * pow((1 - x) / (1 - xhat), alphaB);
 
-      // Correction to 'b' due to the bmin prescription. Notice that
-      // the non-perturbative function does not tend to one anymore
-      // for 'b' tending to zero. (Not sure this is correct).
-      //const double bmin  = 2 * exp( - apfel::emc) / sqrt(zeta);
-      //const double power = 4;
-      //const double bc    = b / pow( ( 1 - exp( - pow(b / bmin, power) ) ), 1 / power);
-
-      //return exp( - ( g1 + g2 * log(zeta / Q02) / 2 ) * bc * bc ) * ( 1 - lambda * pow(g1 * bc / 2, 2) / ( 1 + lambda * g1 ) );
-      //return exp( - ( g1 + g2 * log(zeta / Q02) / 2 ) * b * b ) * ( 1 - lambda * pow(g1 * b / 2, 2) / ( 1 + lambda * g1 ) );
-      return 1 / ( 1 + pow(g1 * b, 2) ) * exp( - g2 * log(zeta / Q02) * b * b / 2 );
-    };
+      return (
+       	      (1-pow(lambdaB,2)) /(pow(1 +  g1 / 4  * b * b ,1)))
+       	+  pow(lambdaB,2)
+       	* (( g1B * exp( - g1B / 4  * b * b )
+             + pow(lambdaC,2)* pow(g1C,2) * ( 1 - g1C/4  * b * b )
+       	    * exp( - g1C / 4  * b * b ))
+       	/ (g1B + pow(lambdaC,2) * pow(g1C,2))
+       	   )
+       	* exp( - g2 * log(zeta / Q02) * pow(b, beta) / 4 
+      	       - g2B * log(zeta / Q02) * pow(b, 4) / 4
+	       );
+     };
 
     std::string LatexFormula() const
     {
       std::string formula;
-      formula  = R"delimiter($$f_{\rm NP}(x,\zeta, b_T)=\frac{\exp\left[ - \frac{1}{2} g_2 \log\left(\frac{\zeta}{Q_0^2}\right) b_T^2 \right]}{1 + g_1^2(x) b_T^2}$$\\)delimiter";
+      formula  = R"delimiter($$f_{\rm NP}(x,\zeta, b_T)= \Biggl(
+\frac{1-\lambda_B^2}{1 + g_1^2(x) b_T^2/4} + \lambda_B^2 \exp \left(-g_{1B} b_T^2 /4 \right)\Biggr) \exp\left[- g_2 \log\left(\frac{\zeta}{Q_0^2}\right) b_T^2/4 - g_{2B} \log\left(\frac{\zeta}{Q_0^2}\right) b_T^4/4 \right]$$\\)delimiter";
       formula += R"delimiter($$g_1(x) = N_1 \frac{x^{\sigma}(1-x)^{\alpha}}{\hat{x}^{\sigma}(1-\hat{x})^{\alpha}}$$\\)delimiter";
+      formula += R"delimiter($$g_{1B}(x) = N_{1B} \frac{x^{\sigma_B}(1-x)^{\alpha_B}}{\hat{x}^{\sigma_B}(1-\hat{x})^{\alpha_B}}$$\\)delimiter";
       formula += R"delimiter($$Q_0^2 = 1\;{\rm GeV}^2$$\\)delimiter";
       formula += R"delimiter($$\hat{x} = 0.1$$)delimiter";
       return formula;
@@ -71,8 +83,16 @@ namespace NangaParbat
               R"delimiter($N_1$)delimiter",
               R"delimiter($\alpha$)delimiter",
               R"delimiter($\sigma$)delimiter",
-              R"delimiter($\lambda$)delimiter",
-              R"delimiter($\delta$)delimiter"};
+              R"delimiter($\delta$)delimiter",
+              R"delimiter($\lambda_B$)delimiter",
+              R"delimiter($N_{1B}$)delimiter",
+              R"delimiter($\alpha_B$)delimiter",
+              R"delimiter($\sigma_B$)delimiter",
+	      R"delimiter($\delta_B$)delimiter",
+	      R"delimiter($\lambda_C$)delimiter",
+              R"delimiter($g_{1C}$)delimiter",
+              R"delimiter($g_{2B}$)delimiter",	      
+              R"delimiter($\beta$)delimiter"};
     };
   };
 }
