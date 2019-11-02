c8beff1058dde594681ee0c3286e5aa069bd0da3
Valerio Bertone <valerio.bertone@cern.ch>
2019-11-02 22:55:46 +0100

    small adjustment

--- cli/tables.py
+++ cli/tables.py
@@ -401,7 +401,7 @@ elif answer["Host"] == "Slurm":
     f.write("#SBATCH --array=0-" + str(len(selds)-1) + "\n")
     f.write("#SBATCH --partition=12hr\n")
     f.write("\n")
-    f.write("DS = ( ")
+    f.write("DS=( ")
     for s in selds:
         f.write(s + " ")
     f.write(")\n\n")
