82d5127008f594885f36ca7b23ef3a08a8046e34
Valerio Bertone <valerio.bertone@cern.ch>
2020-08-18 15:48:43 +0200

    Cleaning up

--- bash/cancel_jobs.sh
+++
@@ -1,9 +0,0 @@
-
-TMDtype="pdf"
-#TMDtype=ff
-
-for (( r=0; r<=300; r++ ))
-do
-    scancel --name=grid_${TMDtype}_$r
-done
-
--- bash/start_jobs.sh
+++
@@ -1,4 +0,0 @@
-for (( m=0; m<=300; m++ ))
-    do
-        sbatch job_$m.sh
-    done
