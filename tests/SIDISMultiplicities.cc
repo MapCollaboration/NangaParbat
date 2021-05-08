e645c36ff161301557dac753627dbed496954b18
Chiara Bissolotti <chiara.bissolotti01@universitadipavia.it>
2021-05-08 18:35:16 +0200

    update predictions at the average values

--- tests/SIDISMultiplicities.cc
+++ tests/SIDISMultiplicities.cc
@@ -25,7 +25,7 @@ int main(int argc, char* argv[])
   if (argc < 4 || strcmp(argv[1], "--help") == 0)
     {
       std::cout << "\nUsage:" << std::endl;
-      std::cout << "Syntax: ./SIDISMultiplicities <YAML configuration file> <path to data folder> <output folder>\n" << std::endl;
+      std::cout << "Syntax: ./SIDISMultiplicities <YAML configuration file [config.yaml]> <path to data folder> <output folder>\n" << std::endl;
       exit(-10);
     }
 
@@ -41,19 +41,14 @@ int main(int argc, char* argv[])
   std::cout << "\033[1;33mComputation done at the average values, NOT INTEGRATED.\033[0m" << std::endl;
 
   // Perturbative order
-  // const int PerturbativeOrder = 3;
   const int PerturbativeOrder = config["PerturbativeOrder"].as<int>();
   std::cout << "\033[1;32mPerturbative order: " << NangaParbat::PtOrderMap.at(PerturbativeOrder) << "\n\033[0m" << std::endl;
 
   // Open LHAPDF sets
-  // LHAPDF::PDF* distpdf = LHAPDF::mkPDF("GJR08FFnloE", config["pdfset"]["member"].as<int>());
-  // LHAPDF::PDF* distpdf = LHAPDF::mkPDF("MMHT2014nnlo68cl", config["pdfset"]["member"].as<int>());
   LHAPDF::PDF* distpdf = LHAPDF::mkPDF(config["pdfset"]["name"].as<std::string>(), config["pdfset"]["member"].as<int>());
-  LHAPDF::PDF* distff  = LHAPDF::mkPDF(config["ffset"]["name"].as<std::string>(), config["ffset"]["member"].as<int>());
 
   // Rotate PDF set into the QCD evolution basis
   const auto RotPDFs = [=] (double const& x, double const& mu) -> std::map<int,double> { return apfel::PhysToQCDEv(distpdf->xfxQ(x, mu)); };
-  const auto RotFFs  = [=] (double const& x, double const& mu) -> std::map<int,double> { return apfel::PhysToQCDEv(distff->xfxQ(x, mu)); };
 
   // Get heavy-quark thresholds from the PDF LHAPDF set
   std::vector<double> Thresholds;
@@ -64,76 +59,58 @@ int main(int argc, char* argv[])
   // Alpha_s (from PDFs). Get it from the LHAPDF set and tabulate it.
   const auto Alphas = [&] (double const& mu) -> double{ return distpdf->alphasQ(mu); };
   const apfel::TabulateObject<double> TabAlphas {[&] (double const& mu) -> double{return distpdf->alphasQ(mu); },
-                                                 100, distpdf->qMin() - 0.5, distpdf->qMax(), 3, Thresholds};
+                                                 100, distpdf->qMin(), distpdf->qMax(), 3, Thresholds};
 
-  // Setup APFEL++ x-space grid for PDFs
-  std::vector<apfel::SubGrid> vsgp;
-  for (auto const& sg : config["xgridpdf"])
-    vsgp.push_back({sg[0].as<int>(), sg[1].as<double>(), sg[2].as<int>()});
-  const apfel::Grid gpdf{vsgp};
+   // Setup APFEL++ x-space grid for PDFs
+   std::vector<apfel::SubGrid> vsgp;
+   for (auto const& sg : config["xgridpdf"])
+     vsgp.push_back({sg[0].as<int>(), sg[1].as<double>(), sg[2].as<int>()});
+   const apfel::Grid gpdf{vsgp};
 
-  // Setup APFEL++ x-space grid for FFs
-  std::vector<apfel::SubGrid> vsgf;
-  for (auto const& sg : config["xgridff"])
-    vsgf.push_back({sg[0].as<int>(), sg[1].as<double>(), sg[2].as<int>()});
-  const apfel::Grid gff{vsgf};
+   // Scale-variation factors
+   const double Ci = config["TMDscales"]["Ci"].as<double>();
+   const double Cf = config["TMDscales"]["Cf"].as<double>();
 
-  // Scale-variation factors
-  const double Ci = config["TMDscales"]["Ci"].as<double>();
-  const double Cf = config["TMDscales"]["Cf"].as<double>();
+   // Get non-perturbative functions
+   const NangaParbat::Parameterisation* fNP = NangaParbat::GetParametersation("PV17");
 
-  // Get non-perturbative functions
-  const NangaParbat::Parameterisation* fNP = NangaParbat::GetParametersation("PV17");
+   // Set cut
+   const double qToQcut = 3;
 
-  // Set cut
-  const double qToQcut = 3;
+   // Electromagnetic coupling squared (provided by APFEL++)
+   const double aref = config["alphaem"]["aref"].as<double>();
+   apfel::AlphaQED alphaem{aref, config["alphaem"]["Qref"].as<double>(), Thresholds, {0, 0, 1.777}, 0};
+   const apfel::TabulateObject<double> TabAlphaem{alphaem, 100, 0.9, 1001, 3};
 
-  // Electromagnetic coupling squared (provided by APFEL++)
-  const double aref = config["alphaem"]["aref"].as<double>();
-  apfel::AlphaQED alphaem{aref, config["alphaem"]["Qref"].as<double>(), Thresholds, {0, 0, 1.777}, 0};
-  const apfel::TabulateObject<double> TabAlphaem{alphaem, 100, 0.9, 1001, 3};
+   // Construct set of distributions as a function of the scale to be tabulated
+   const auto EvolvedPDFs = [=,&gpdf] (double const& mu) -> apfel::Set<apfel::Distribution>
+   {
+     return apfel::Set<apfel::Distribution>{apfel::EvolutionBasisQCD{apfel::NF(mu, Thresholds)}, DistributionMap(gpdf, RotPDFs, mu)};
+   };
 
-  // Construct set of distributions as a function of the scale to be
-  // tabulated
-  const auto EvolvedPDFs = [=,&gpdf] (double const& mu) -> apfel::Set<apfel::Distribution>
-  {
-    return apfel::Set<apfel::Distribution>{apfel::EvolutionBasisQCD{apfel::NF(mu, Thresholds)}, DistributionMap(gpdf, RotPDFs, mu)};
-  };
-  const auto EvolvedFFs = [=,&gff] (double const& mu) -> apfel::Set<apfel::Distribution>
-  {
-    return apfel::Set<apfel::Distribution>{apfel::EvolutionBasisQCD{apfel::NF(mu, Thresholds)}, DistributionMap(gff, RotFFs, mu)};
-  };
+   // Tabulate collinear PDFs
+   const apfel::TabulateObject<apfel::Set<apfel::Distribution>> TabPDFs{EvolvedPDFs, 200, distpdf->qMin(), distpdf->qMax(), 3, Thresholds}; // ATTENTION! In fastinterface.cc, l. 57 there is only distpdf->qMin(). THIS HAS AN IMPACT ON THE FINAL PREDICTIONS! Same for FFs.
+   // const apfel::TabulateObject<apfel::Set<apfel::Distribution>> TabPDFs{EvolvedPDFs, 200, distpdf->qMin() - 0.1, distpdf->qMax(), 3, Thresholds};
+   const auto CollPDFs = [&] (double const& mu) -> apfel::Set<apfel::Distribution> { return TabPDFs.Evaluate(mu); };
 
-  // Tabulate collinear PDFs
-  const apfel::TabulateObject<apfel::Set<apfel::Distribution>> TabPDFs{EvolvedPDFs, 200, distpdf->qMin() - 0.1, distpdf->qMax(), 3, Thresholds}; // ATTENTION! In fastinterface.cc, l. 57 there is only distpdf->qMin(). THIS HAS AN IMPACT ON THE FINAL PREDICTIONS! Same for FFs.
-  const auto CollPDFs = [&] (double const& mu) -> apfel::Set<apfel::Distribution> { return TabPDFs.Evaluate(mu); };
+   // Initialize TMD PDF objects
+   const auto TmdObjPDF = apfel::InitializeTmdObjectsLite(gpdf, Thresholds);
 
-  // Tabulate collinear FFs
-  const apfel::TabulateObject<apfel::Set<apfel::Distribution>> TabFFs{EvolvedFFs, 200, distff->qMin() - 0.1, distff->qMax(), 3, Thresholds};
-  const auto CollFFs = [&] (double const& mu) -> apfel::Set<apfel::Distribution> { return TabFFs.Evaluate(mu); };
+   // Build evolved TMD PDFs
+   const auto EvTMDPDFs    = BuildTmdPDFs(TmdObjPDF, CollPDFs, Alphas, PerturbativeOrder, Ci);
+   const auto MatchTMDPDFs = MatchTmdPDFs(TmdObjPDF, CollPDFs, Alphas, PerturbativeOrder, Ci);
 
-  // Initialize TMD objects
-  const auto TmdObjPDF = apfel::InitializeTmdObjectsLite(gpdf, Thresholds);
-  const auto TmdObjFF  = apfel::InitializeTmdObjectsLite(gff,  Thresholds);
+   auto QuarkSudakov = QuarkEvolutionFactor(TmdObjPDF, Alphas, PerturbativeOrder, Ci, 1e5);
 
-  // Build evolved TMD PDFs
-  const auto EvTMDPDFs    = BuildTmdPDFs(TmdObjPDF, CollPDFs, Alphas, PerturbativeOrder, Ci);
-  const auto MatchTMDPDFs = MatchTmdPDFs(TmdObjPDF, CollPDFs, Alphas, PerturbativeOrder, Ci);
-
-  // Build evolved TMD FFs
-  const auto EvTMDFFs    = BuildTmdFFs(TmdObjFF, CollFFs, Alphas, PerturbativeOrder, Ci);
-  const auto MatchTMDFFs = MatchTmdFFs(TmdObjFF, CollFFs, Alphas, PerturbativeOrder, Ci);
-
-  auto QuarkSudakov = QuarkEvolutionFactor(TmdObjPDF, Alphas, PerturbativeOrder, Ci, 1e5);
-
-  // Get hard-factor
-  const std::function<double(double const&)> Hf = apfel::HardFactor("SIDIS", TmdObjPDF, Alphas, PerturbativeOrder, Cf);
+   // Get hard-factor
+   const std::function<double(double const&)> Hf = apfel::HardFactor("SIDIS", TmdObjPDF, Alphas, PerturbativeOrder, Cf);
 
   // Functions used for the tabulation
   const auto TabFunc    = [] (double const& b) -> double{ return log(b); };
   const auto InvTabFunc = [] (double const& fb) -> double{ return exp(fb); };
 
-  // Initialise GSL random-number generator, essential to initialize DataHandler object
+  // Initialise GSL random-number generator
+  // (essential to initialize DataHandler object)
   gsl_rng *rng = gsl_rng_alloc(gsl_rng_ranlxs2);
   gsl_rng_set(rng, 1234);
 
@@ -166,12 +143,62 @@ int main(int argc, char* argv[])
     {
       std::cout << "\033[1;37m\nComputing SIDIS multiplicities for: " << ds["name"].as<std::string>() << "...\033[0m" << std::endl;
 
+      std::string FFset;
+      // Choose the correct FFs set depending on the experiment.
+      if (ds["name"].as<std::string>().substr(0, 18) == "COMPASS_Deu_Pp_Kp_")
+        FFset = "DSS07_NLO_HadronPlus";
+      else if (ds["name"].as<std::string>().substr(0, 18) == "COMPASS_Deu_Pm_Km_")
+        FFset = "DSS07_NLO_HadronMinus";
+      else if (ds["name"].as<std::string>().substr(0, 14) == "HERMES_Pro_Pip" || ds["name"].as<std::string>().substr(0, 14) == "HERMES_Deu_Pip")
+        FFset = "DSS14_NLO_Pip";
+      else if (ds["name"].as<std::string>().substr(0, 14) == "HERMES_Pro_Pim" || ds["name"].as<std::string>().substr(0, 14) == "HERMES_Deu_Pim")
+        FFset = "DSS14_NLO_Pim";
+      else if (ds["name"].as<std::string>().substr(0, 13) == "HERMES_Pro_Kp" || ds["name"].as<std::string>().substr(0, 13) == "HERMES_Deu_Kp")
+        FFset = "DSS17_NLO_KaonPlus";
+      else if (ds["name"].as<std::string>().substr(0, 13) == "HERMES_Pro_Km" || ds["name"].as<std::string>().substr(0, 13) == "HERMES_Deu_Km")
+        FFset = "DSS17_NLO_KaonMinus";
+      else
+        std::cout << "\033[1;31m [SIDISMultiplicities] Unknown SIDIS Experiment - FFs not initialized\033[0m" << std::endl;
+
+      // Open LHAPDF FFs sets
+      // FFset = config["ffset"]["name"].as<std::string>();
+      LHAPDF::PDF* distff  = LHAPDF::mkPDF(FFset, config["ffset"]["member"].as<int>());
+
+      // Rotate FF set into the QCD evolution basis
+      const auto RotFFs  = [=] (double const& x, double const& mu) -> std::map<int,double> { return apfel::PhysToQCDEv(distff->xfxQ(x, mu)); };
+
+      // Setup APFEL++ x-space grid for FFs
+      std::vector<apfel::SubGrid> vsgf;
+      for (auto const& sg : config["xgridff"])
+        vsgf.push_back({sg[0].as<int>(), sg[1].as<double>(), sg[2].as<int>()});
+      const apfel::Grid gff{vsgf};
+
+      // Construct set of distributions as a function of the scale to be
+      // tabulated
+      const auto EvolvedFFs = [=,&gff] (double const& mu) -> apfel::Set<apfel::Distribution>
+      {
+        return apfel::Set<apfel::Distribution>{apfel::EvolutionBasisQCD{apfel::NF(mu, Thresholds)}, DistributionMap(gff, RotFFs, mu)};
+      };
+
+      // Tabulate collinear FFs
+      const apfel::TabulateObject<apfel::Set<apfel::Distribution>> TabFFs{EvolvedFFs, 200, distff->qMin(), distff->qMax(), 3, Thresholds};
+      // const apfel::TabulateObject<apfel::Set<apfel::Distribution>> TabFFs{EvolvedFFs, 200, distff->qMin() - 0.1, distff->qMax(), 3, Thresholds};
+      const auto CollFFs = [&] (double const& mu) -> apfel::Set<apfel::Distribution> { return TabFFs.Evaluate(mu); };
+
+      // Initialize TMD FF objects
+      const auto TmdObjFF  = apfel::InitializeTmdObjectsLite(gff,  Thresholds);
+
+      // Build evolved TMD FFs
+      const auto EvTMDFFs    = BuildTmdFFs(TmdObjFF, CollFFs, Alphas, PerturbativeOrder, Ci);
+      const auto MatchTMDFFs = MatchTmdFFs(TmdObjFF, CollFFs, Alphas, PerturbativeOrder, Ci);
+
+      // Start reading datafiles
       const YAML::Node datafile = YAML::LoadFile(std::string(argv[2]) + "/" + exper.first.as<std::string>() + "/" + ds["file"].as<std::string>());
       NangaParbat::DataHandler* dh = new NangaParbat::DataHandler{ds["name"].as<std::string>(), datafile,
                                                                   rng, ReplicaID,
                                                                   std::vector<double>{}};
 
-      // Keep track is the multiplicities are from HERMES or COMPASS
+      // Keep track if the multiplicities are from HERMES or COMPASS
       bool COMPASS = (exper.first.as<std::string>() == "COMPASS" ? true : false);
 
       // Kinematics for the fully differential computation
@@ -185,11 +212,15 @@ int main(int argc, char* argv[])
 
       // Get Q from the fist point, (Q is constant in each bin)
       const double Qav  = dh->GetBinning()[0].Qav;
+      // const double Qav  = dh->GetKinematics().var1b.first; // Q min
+      // const double Qav  = dh->GetKinematics().var1b.second; // Q max
 
       // Get z (which for HERMES changes slightly in a bin)
       std::vector<double> zv(dh->GetKinematics().ndata);
       for (int j = 0; j < dh->GetKinematics().ndata; j++)
-        zv[j] = dh->GetBinning()[j].zav;
+        // zv[j] = dh->GetBinning()[j].zav;
+        // zv[j] = dh->GetKinematics().var3b.first; // z min
+        zv[j] = dh->GetKinematics().var3b.second; // z max
 
       // Get PhT values
       std::vector<double> PhTv;
@@ -208,7 +239,6 @@ int main(int argc, char* argv[])
 
       // Get plotting labels
       const std::map<std::string, std::string> labels = dh->GetLabels();
-
 
       // Taking into account the isoscalarity of the target
       const int sign = (targetiso >= 0 ? 1 : -1);
@@ -269,10 +299,10 @@ int main(int argc, char* argv[])
 
       // Denominator of multiplicities: compute inclusive cross section.
       // First adjust PDFs to account for the isoscalarity.
-      const std::function<std::map<int, double>(double const&, double const&)> tPDFs = [&] (double const& xm, double const& Qm) -> std::map<int, double>
+      const std::function<std::map<int, double>(double const&, double const&)> tPDFs = [&] (double const& x, double const& Q) -> std::map<int, double>
       {
         // Get PDFs in the physical basis
-        const std::map<int, double> pr = apfel::QCDEvToPhys(TabPDFs.EvaluateMapxQ(xm, Qm));
+        const std::map<int, double> pr = apfel::QCDEvToPhys(TabPDFs.EvaluateMapxQ(x, Q));
         std::map<int, double> tg = pr;
         // Apply isoscalarity
         tg.at(1)  = frp * pr.at(1)  + frn * pr.at(2);
@@ -332,6 +362,7 @@ int main(int argc, char* argv[])
     		  const double Qm  = Qav;
     		  const double xm  = xav;
     		  const double zm  = zv[iqT];
+    		  // const double zm  = zv[6];
 
           // Scales
     		  const double mu  = Cf * Qm;
@@ -344,46 +375,6 @@ int main(int argc, char* argv[])
 
           // Number of active flavors
     		  const int nf = apfel::NF(mu, Thresholds);
-
-          // // Alphaem
-    		  // const double aem2 = pow((config["alphaem"]["run"].as<bool>() ? alphaem.Evaluate(Qm) : aref), 2);
-
-          /*
-          // Luminosity without taking into account isoscalarity
-          const std::function<apfel::DoubleObject<apfel::Distribution>(double const&)> Lumib = [=] (double const& bs) -> apfel::DoubleObject<apfel::Distribution>
-    		    {
-    		      const std::map<int, apfel::Distribution> xF = QCDEvToPhys(EvTMDPDFs(bs, mu, zeta).GetObjects());
-    		      const std::map<int, apfel::Distribution> xD = QCDEvToPhys(EvTMDFFs(bs, mu, zeta).GetObjects());
-    		      apfel::DoubleObject<apfel::Distribution> L{};
-    		      for (int i = 1; i <= nf; i++)
-          			{
-          			  L.AddTerm({Bq[i-1], xF.at(+i), xD.at(-i)});
-          			  L.AddTerm({Bq[i-1], xF.at(-i), xD.at(+i)});
-          			}
-    		      return L;
-    		    };
-
-          // Integrand in bT space
-    		  const double overflow = 1.2;
-    		  const double bmin = NangaParbat::bstarmin(0.00001, Qm) / overflow;
-    		  const double bmax = NangaParbat::bstarmin(10, Qm) * overflow;
-    		  const apfel::TabulateObject<apfel::DoubleObject<apfel::Distribution>> tLumib{Lumib, 200, bmin, bmax, 3, {}, TabFunc, InvTabFunc};
-
-    		  const apfel::TabulateObject<double> tf1NP{[=] (double const& tb) -> double { return fNP->Evaluate(xm, tb, zeta, 0); },
-    		      100, 5e-5, 5, 3, {}, TabFunc, InvTabFunc};
-    		  const apfel::TabulateObject<double> tf2NP{[=] (double const& tb) -> double { return fNP->Evaluate(zm, tb, zeta, 1); },
-    		      100, 5e-5, 5, 3, {}, TabFunc, InvTabFunc};
-
-
-    		  const std::function<double(double const&)> bInt = [=] (double const& b) -> double
-    		    {
-    		      return b * tf1NP.Evaluate(b) * tf2NP.Evaluate(b) * tLumib.EvaluatexzQ(xm, zm, NangaParbat::bstarmin(b, Qm));
-    		    };
-
-
-          // Differential cross section
-    		  const double differ = apfel::ConvFact * apfel::FourPi * aem2 * qTm * Yp * Hf(mu) * DEObj.transform(bInt, qTm) / pow(Qm, 3) / xm / zm / zm ;
-          */
 
           const apfel::TabulateObject<double> tf1NP{[=] (double const& tb) -> double { return fNP->Evaluate(xm, tb, zeta, 0); },
               100, 5e-5, 5, 3, {}, TabFunc, InvTabFunc};
@@ -465,6 +456,7 @@ int main(int argc, char* argv[])
         std::cout << "Total computation time, ";
         t.stop(true);
 
+        delete distff;
     }
 
   // Output file
@@ -473,7 +465,6 @@ int main(int argc, char* argv[])
   fout.close();
 
   delete distpdf;
-  delete distff;
   delete fNP;
 
   // Delete random-number generator
