26fd7f83504f1aa66606ecea0c4f3d2868c8bb02
vbertone <valerio.bertone@gmail.com>
2021-12-11 10:04:27 +0100

    Fixing warnings

--- inc/NangaParbat/convolutiontable.h
+++ inc/NangaParbat/convolutiontable.h
@@ -165,8 +165,8 @@ namespace NangaParbat
      */
     ///@{
   public:
-    virtual void SetInputFFs(std::function<std::map<int, double>(double const &, double const &)> const &InDistFunc) {};
-    virtual void SetInputFFs(std::function<apfel::Set<apfel::Distribution>(double const&)> const& InDistFunc) {};
+    virtual void SetInputFFs(std::function<std::map<int, double>(double const &, double const &)> const&) {};
+    virtual void SetInputFFs(std::function<apfel::Set<apfel::Distribution>(double const&)> const&) {};
     virtual std::vector<double> GetPredictions(std::function<double(double const&, double const&, double const&)> const&) const { return {}; };
     ///@}
   };
