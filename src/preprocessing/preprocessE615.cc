e9f6c061946947921f3d3f82123500a94dcb7806
SimoV <>
2022-04-29 15:05:58 +0200

    Preprocesssing OK

--- src/preprocessing/preprocessE615.cc
+++ src/preprocessing/preprocessE615.cc
@@ -154,9 +154,9 @@ namespace NangaParbat
             std::map<std::string, std::string> labels
             {
               {"xlabel", "#it{q}_{T} [GeV]"},
-              {"ylabel", "#frac{d2#it{#sigma}}{d#it{Q}#d#it{q}_{T}} [pb*GeV^{-2}"},
+              {"ylabel", "#frac{d2#it{#sigma}}{d#it{Q}#d#it{q}_{T}} [pb*GeV^{-2}]"},
               {"title", "E615 at 252 GeV, " + std::to_string(Qb.second.first) + " < Q < " + std::to_string(Qb.second.second)},
-              {"xlabelpy", "$q_T \\rm{[GeV]$"},
+              {"xlabelpy", "$q_T \\rm{[GeV]}$"},
               {"ylabelpy", "$\\frac{d^2\\sigma}{dQ dq_{T}}[\\rm{pb}*{GeV}^{-2}]$"},
               {"titlepy", "E615 at 252 GeV, " + std::to_string(Qb.second.first) + " < Q < " + std::to_string(Qb.second.second)}
             };
