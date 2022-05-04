4a4a34a935e0f7d47299a896f147623600f9c479
lorenzorossi97 <lorelore97@gmail.com>
2022-05-04 17:30:24 +0200

    commit di parti non perturbative

--- inc/NangaParbat/MAPTMDPion.h
+++ inc/NangaParbat/MAPTMDPion.h
@@ -19,8 +19,7 @@ namespace NangaParbat
   public:
 
     MAPTMDPion():
-    Parameterisation{"MAPTMDPion", 2, std::vector<double> {0, 0, 0, 0, 0, 0, 0}}
-    {};
+    Parameterisation{"MAPTMDPion", 2, std::vector<double> {0.2343, 0.2788, 1.3806, 0.8466, 1.9872, 0.0051, 10.0594, 0.0106, 1.4513, 0.0871, 0.2053, 0.1589, 0.2513}} { };
 
     double Evaluate(double const& x, double const& b, double const& zeta, int const& ifunc) const
     {
@@ -35,23 +34,32 @@ namespace NangaParbat
         const double g2   = this->_pars[0];
         const double evol = exp( - g2 * log(zeta / _Q02) * b * b / 4 );
 
-        // TMD PDFs
-        if (ifunc == 0)
+        if (ifunc == 0)   // TMD PDFs proton MAP22
           {
-            const double N1     = this->_pars[1];
-            const double alpha  = this->_pars[2];
-            const double sigma  = this->_pars[3];
-            const double lambda = this->_pars[4];
-            const double xhat   = 0.1;
-            const double g1     = N1 * pow(x / xhat, sigma) * pow((1 - x) / (1 - xhat), alpha);
-            return evol * exp( - g1 * pow(b / 2, 2) ) * ( 1 - lambda * pow(g1 * b / 2, 2) / ( 1 + lambda * g1 ) );
+            const double N1      = this->_pars[1];
+            const double sigma1  = this->_pars[2];
+            const double alpha1  = this->_pars[3];
+            const double lambda  = this->_pars[4];
+            const double N1B     = this->_pars[5];
+            const double sigma2  = this->_pars[6];
+            const double alpha2  = this->_pars[7];
+            const double lambda2 = this->_pars[8];
+            const double N1C     = this->_pars[9];
+            const double sigma3  = this->_pars[10];
+            const double alpha3  = this->_pars[11];
+            const double xhat    = 0.1;
+            const double g1      = N1 * pow(x / xhat, sigma1) * pow((1 - x) / (1 - xhat), pow(alpha1, 2));
+  	        const double g1B     = N1B * pow(x / xhat, sigma2) * pow((1 - x) / (1 - xhat), pow(alpha2, 2));
+            const double g1C     = N1C * pow(x / xhat, sigma3) * pow((1 - x) / (1 - xhat), pow(alpha3, 2));
+  	        return evol * ( g1 * exp( - g1 * pow(b / 2, 2))
+                            +  pow(lambda, 2)  * pow(g1B, 2) * ( 1 - g1B * pow(b / 2, 2)) * exp( - g1B * pow(b / 2, 2)) + g1C * pow(lambda2, 2) * exp( - g1C * pow(b / 2, 2)))
+                   / ( g1 +  pow(lambda, 2)  * pow(g1B, 2) + g1C * pow(lambda2, 2));
           }
-      else
-	{
-    const double N1pi     = this->_pars[5];
-    const double alphapi  = this->_pars[6];
-	  return evol * N1pi * exp( - pow(alphapi * b / 2, 2) );
-	}
+        else    // TMD PDFs pion
+        	{
+            const double alphapi  = this->_pars[12];
+        	  return evol  * exp( - pow(alphapi * b / 2, 2) );
+        	}
 
     };
 
@@ -70,16 +78,22 @@ namespace NangaParbat
     {
       return {R"delimiter($g_2$)delimiter",
               R"delimiter($N_1$)delimiter",
-              R"delimiter($\alpha$)delimiter",
-              R"delimiter($\sigma$)delimiter",
+              R"delimiter($\sigma1$)delimiter",
+              R"delimiter($\alpha1$)delimiter",
               R"delimiter($\lambda$)delimiter",
-              R"delimiter($N_1pi$)delimiter",
+              R"delimiter($N_1B$)delimiter",
+              R"delimiter($\sigma2$)delimiter",
+              R"delimiter($\alpha2$)delimiter",
+              R"delimiter($\lambda2$)delimiter",
+              R"delimiter($N1C$)delimiter",
+              R"delimiter($\sigma3$)delimiter",
+              R"delimiter($\alpha3$)delimiter",
               R"delimiter($\alphapi$)delimiter"};
     };
 
     std::string GetDescription() const
     {
-      return "Parameterisation used for the Pavia 2019 TMD analysis.";
+      return "Parameterisation used for the Pavia 2022 TMD analysis.";
     };
 
   private:
