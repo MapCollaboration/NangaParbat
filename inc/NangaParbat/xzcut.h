e0fdad530ba288053c7a97463e3489fc1b794db2
vbertone <valerio.bertone@gmail.com>
2022-08-22 18:25:54 +0200

    Introducing log cut in x-z

--- inc/NangaParbat/xzcut.h
+++ inc/NangaParbat/xzcut.h
@@ -10,7 +10,7 @@ namespace NangaParbat
 {
   /**
    * @brief Derivation of the class Cut to impose a cut on the variable
-   * "z" as a function of x.
+   * "z" as a linear function of x.
    */
   class XZCut: public Cut
   {
@@ -34,14 +34,14 @@ namespace NangaParbat
   };
 
   /**
-   * @brief Derivation of the class Cut to impose yet a different cut
-   * on the variable "z" as a function of x.
+   * @brief Derivation of the class Cut to impose a cut on the
+   * variable "z" as a quadratic function of x.
    */
   class XZCutQuadratic: public Cut
   {
   public:
     /**
-     * @brief The "XZcut" constructor
+     * @brief The "XZCutQuadratic" constructor
      * @param dataset: the DataHandler object subject to the cuts
      * @param betamin: mininum value
      * @param betamax: maximum value
@@ -53,5 +53,30 @@ namespace NangaParbat
      * derived class to eforce the cut.
      */
     void EnforceCut();
+  };
+
+  /**
+   * @brief Derivation of the class Cut to impose a cut on the variable
+   * "z" as a logarithmic function of x.
+   */
+  class XZCutLog: public Cut
+  {
+  public:
+    /**
+     * @brief The "XZCutLog" constructor
+     * @param dataset: the DataHandler object subject to the cuts
+     * @param betamin: mininum value
+     * @param betamax: maximum value
+     * @param alpha: angular coefficient
+     */
+    XZCutLog(DataHandler const& dataset, double const& betamin, double const& betamax, double const& alpha);
+
+    /**
+     * @brief Purely virtual function to be used implemented in the
+     * derived class to eforce the cut.
+     */
+    void EnforceCut();
+  private:
+    double const _alpha;
   };
 }
--- src/cuts/cutfactory.cc
+++ src/cuts/cutfactory.cc
@@ -20,6 +20,8 @@ namespace NangaParbat
       return std::shared_ptr<NangaParbat::Cut> {new NangaParbat::XZCut{dataset, min, max, params[0]}};
     else if (name == "xzcutQuad")
       return std::shared_ptr<NangaParbat::Cut> {new NangaParbat::XZCutQuadratic{dataset, min, max}};
+    else if (name == "xzcutLog")
+      return std::shared_ptr<NangaParbat::Cut> {new NangaParbat::XZCutLog{dataset, min, max, params[0]}};
     else
       throw std::runtime_error("[CutFactory::GetInstance]: Unknown cut.");
   }
