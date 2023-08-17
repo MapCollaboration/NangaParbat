489afead6adab82cee2f3aa31c017c5efb95e10c
Valerio Bertone <valerio.bertone@cern.ch>
2023-08-17 12:53:27 +0200

    small adjustment to cut in W

--- src/cuts/wcut.cc
+++ src/cuts/wcut.cc
@@ -19,35 +19,20 @@ namespace NangaParbat
     const std::vector<DataHandler::Binning> bins = _dataset.GetBinning();
     _mask.resize(bins.size());
 
-    double W;
     for (int i = 0; i < (int) bins.size(); i++)  
       {
-        W = pow(apfel::ProtonMass, 2);
+        double W2 = pow(apfel::ProtonMass, 2);
         if (bins[i].Intx)
           if (bins[i].IntQ)
-            {
-              W += pow(bins[i].Qmin, 2) * (1/bins[i].xmax - 1);
-              W = sqrt(W);
-              _mask[i] = ( W - _min > 0 && W - _max < 0 ? true : false);
-            }
+	    W2 += pow(bins[i].Qmin, 2) * ( 1 / bins[i].xmax - 1 );
           else
-            {
-              W += pow(bins[i].Qav,2) * (1/bins[i].xmax - 1);
-              W = sqrt(W);
-              _mask[i] = ( W - _min > 0 && W - _max < 0 ? true : false);
-            }
-        else if (bins[i].IntQ)
-          {
-            W += pow(bins[i].Qmin, 2) * (1/bins[i].xav - 1);
-            W = sqrt(W);
-            _mask[i] = ( W - _min > 0 && W - _max < 0 ? true : false);
-          }
-        else 
-          {
-            W = pow(bins[i].Qav, 2) * (1/bins[i].xav - 1);
-            W = sqrt(W);
-            _mask[i] = (W > _min && W < _max ? true : false); 
-          } 
-      }    
+	    W2 += pow(bins[i].Qav, 2) * ( 1 / bins[i].xmax - 1 );
+        else
+	  if (bins[i].IntQ)
+	    W2 += pow(bins[i].Qmin, 2) * ( 1 / bins[i].xav - 1 );
+	  else
+	    W2 += pow(bins[i].Qav, 2) * ( 1 /bins[i].xav - 1 );
+	_mask[i] = (sqrt(W2) > _min && sqrt(W2) < _max ? true : false);
+      }
   }
 }
